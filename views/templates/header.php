63ad37cb50428cb389eeab3648d8bd8740395ebc
seem-less <mgirach@gmail.com>
2020-01-22 18:28:25 +0100

    change site title

--- views/templates/header.php
+++ views/templates/header.php
@@ -2,7 +2,7 @@
         <head>
                 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
                 <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />    
-                <title>Onetool Registration Page</title>
+                <title>Registration application</title>
         </head>
         <body>
                 <div class="container">
